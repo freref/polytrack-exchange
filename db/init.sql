705c202f69bf113ebb69597c4b04218f90e4d64f
frehml <frederic.hamelink@hotmail.com>
2024-10-19 03:25:37 +0200

    hashing pw

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,15 @@
 CREATE TABLE
-    IF NOT EXISTS track (
+    IF NOT EXISTS tracks (
         id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         code TEXT NOT NULL,
         upvote integer NOT NULL,
         downvote integer NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS users (
+        username VARCHAR(25) PRIMARY KEY,
+        email VARCHAR(50),
+        password_hash VARCHAR(60)
     );
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type Error struct {
+	ErrorMessage string
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	dbURL := os.Getenv("DATABASE_URL")
+	dbpool, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("./templates/index.html"))
@@ -23,8 +39,12 @@ func main() {
 	}
 
 	hLoginSubmit := func(w http.ResponseWriter, r *http.Request) {
+		data := Error{
+			ErrorMessage: "User does not exist",
+		}
+
 		tmpl := template.Must(template.ParseFiles("./templates/login.html"))
-		tmpl.Execute(w, nil)
+		tmpl.ExecuteTemplate(w, "login-error-block", data)
 	}
 
 	hRegister := func(w http.ResponseWriter, r *http.Request) {
@@ -32,15 +52,53 @@ func main() {
 		tmpl.Execute(w, nil)
 	}
 
+	hRegisterSubmit := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("hRegisterSubmit \n")
+
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Printf("ERROR parse")
+		}
+
+		username := r.FormValue("username")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			fmt.Printf("ERROR password")
+		}
+
+		fmt.Println(username)
+		fmt.Println(email)
+		fmt.Println(password)
+		fmt.Println(string(hash))
+
+		sql := `INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)`
+
+		_, err = dbpool.Exec(context.Background(), sql, username, email, string(hash))
+		if err != nil {
+			fmt.Printf("ERROR inserting user into DB: %v\n", err)
+		}
+
+		var res string
+		_ = dbpool.QueryRow(context.Background(), "SELECT * FROM users").Scan(&res)
+
+		fmt.Println(res)
+	}
+
 	hNav := func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/components/nav.html")
 	}
+
+	fmt.Printf("main \n")
 
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/nav", hNav)
 	http.HandleFunc("/login", hLogin)
 	http.HandleFunc("/login/submit", hLoginSubmit)
 	http.HandleFunc("/register", hRegister)
+	http.HandleFunc("/register/submit", hRegisterSubmit)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
